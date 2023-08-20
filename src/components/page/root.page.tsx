5f161a4345a4ad4629e3b186b9382782c7de329e
tachigui <tachiguigui@gmail.com>
2023-08-20 23:24:00 +0900

    feat: :sparkles: リンク追加

--- src/components/page/root.page.tsx
+++ src/components/page/root.page.tsx
@@ -5,12 +5,18 @@ const RootPage: FC = () => (
   <div className="grid h-screen w-screen place-content-center place-items-center">
     <div className="flex flex-col items-center gap-4 font-ss-en text-3xl font-bold">
       <span>Root.</span>
-      <div className="flex flex-col items-center gap-1">
+      <div className="flex flex-col items-center gap-2">
         <Link
           className="text-blue-600 hover:text-blue-400 hover:underline hover:underline-offset-4"
           to="/headlines"
         >
           Headlines
+        </Link>
+        <Link
+          className="text-blue-600 hover:text-blue-400 hover:underline hover:underline-offset-4"
+          to="/animations"
+        >
+          Animations
         </Link>
       </div>
     </div>
